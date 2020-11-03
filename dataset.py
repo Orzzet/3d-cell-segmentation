5a3e2bb9a8c15cb9766d330e869c73ece76cdfd6
Orzzet <orzzet@gmail.com>
2020-11-03 15:40:03 +0000

    fiji usable dataset correctly loaded with DataLoader

--- dataset.py
+++ dataset.py
@@ -8,15 +8,15 @@ import numpy as np
 
 PATHS = {
 'RAW' : 'data/cells/raw/',
-'LQ_TRAIN' : 'data/cells/0.25z 0.125x 0.125y/train',
-'LQ_VALID' : 'data/cells/0.25z 0.125x 0.125y/valid',
-'LQ_TEST' : 'data/cells/0.25z 0.125x 0.125y/test',
-'MQ_TRAIN' : 'data/cells/0.5z 0.25x 0.25y/train',
-'MQ_VALID' : 'data/cells/0.5z 0.25x 0.25y/valid',
-'MQ_TEST' : 'data/cells/0.5z 0.25x 0.25y/test',
-'HQ_TRAIN' : 'data/cells/0.5z 0.5x 0.5y/train',
-'HQ_VALID' : 'data/cells/0.5z 0.5x 0.5y/valid',
-'HQ_TEST' : 'data/cells/0.5z 0.5x 0.5y/test',
+'LQ_TRAIN' : 'data/cells/0.25z 0.125x 0.125y/train/',
+'LQ_VALID' : 'data/cells/0.25z 0.125x 0.125y/valid/',
+'LQ_TEST' : 'data/cells/0.25z 0.125x 0.125y/test/',
+'MQ_TRAIN' : 'data/cells/0.5z 0.25x 0.25y/train/',
+'MQ_VALID' : 'data/cells/0.5z 0.25x 0.25y/valid/',
+'MQ_TEST' : 'data/cells/0.5z 0.25x 0.25y/test/',
+'HQ_TRAIN' : 'data/cells/0.5z 0.5x 0.5y/train/',
+'HQ_VALID' : 'data/cells/0.5z 0.5x 0.5y/valid/',
+'HQ_TEST' : 'data/cells/0.5z 0.5x 0.5y/test/',
 
 }
 
@@ -26,8 +26,10 @@ class CellsDataset(VisionDataset):
     def __init__(self, folder_path, target_mode='target', dim_size_reduction=(1,1,1), transform=None, transform_augmentation=None):
         """
         Args:
-            transform (callable, optional): Optional transform to be applied
-                on a sample.
+            dim_size_reduction (Tuple[Int, Int, Int], optional): patches size, with (1,1,1) there will be one patch with the same size of the original
+                image, with (0.5, 0.5, 0.5) there will be 8 patches, each patch with half the size on each dimension.
+            transform (callable, optional): Optional transform to be applied on a original image.
+            transform_augmentation (callable, optional): Optional transform to be applied on a both original and target image.
         """
         self.files = [(folder_path + filename, (i,j,k)) for filename in list(os.walk(folder_path))[0][2]
                       for i in range(dim_size_reduction[0]) for j in range(dim_size_reduction[1]) for k in range(dim_size_reduction[2])]
@@ -46,7 +48,9 @@ class CellsDataset(VisionDataset):
         return image[:,i*x:(i+1)*x,j*y:(j+1)*y,k*z:(k+1)*z]
     
     def __getitem__(self, index):
+        # Load file with all images
         current_file = h5py.File(self.files[index][0], 'r')
+        # Get cell count for original and for 
         correct_cell_count, resized_cell_count = current_file.attrs['correct_cell_count'], current_file.attrs['cell_count']
         image = current_file.get('image')
         #image = self.reduce_dims(image, index)
@@ -59,14 +63,17 @@ class CellsDataset(VisionDataset):
         elif self.target_mode == 'target':
             target = current_file.get('target')
         #target = self.reduce_dims(target, index)
-        target = np.array(target, dtype='i1')
+        target = np.array(target, dtype='i2')
         image = Variable(torch.tensor(image))
+        # Image is originally (z,x,y), we need to convert it to (c=1,x,y,z) to use the torchio transforms, where c is number of channels
+        image = image.permute(1,2,0).unsqueeze(0)
         current_file.close()
         if self.transform:
             image = self.transform(image)
+        # We need to convert back from (1,x,y,z) to (1,z,x,y) to use kornia data augmentation properly
         image = image.permute(0,3,1,2)
-        target = np.moveaxis(target, -1, 1)
         target = Variable(torch.tensor(target))
+        target = target.unsqueeze(0)
         if self.transform_augmentation:
             both = torch.stack((image, target))
             image, target = self.transform_augmentation(both)
